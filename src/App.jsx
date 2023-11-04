4556274fe3c89d971e5e41c64d216d601a440839
unknown <yezawhtutmwcdev@gmail.com>
2023-11-04 15:18:23 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Center from "./components/Center";
+
+const App = () => {
+  return (
+    <div>
+      {/* Header Section */}
+
+      <Header />
+
+      {/* Center Section */}
+      <Center />
+    </div>
+  );
+};
+
+export default App;
